50ecc4b1f40c7db0d5bbda4ef868210921c9a4bc
Vlad Yashenkov <yashenkov.v@yandex.ru>
2024-08-13 13:32:57 +0300

    fix: edit settings.py

--- django_fastapi/project/settings.py
+++ django_fastapi/project/settings.py
@@ -89,8 +89,8 @@ TEMPLATES = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "test_db2",
-        # "NAME": DB_NAME,
+        # "NAME": "test_db2",
+        "NAME": DB_NAME,
         "USER": DB_USER,
         "PASSWORD": DB_PASS,
         "HOST": DB_HOST,
