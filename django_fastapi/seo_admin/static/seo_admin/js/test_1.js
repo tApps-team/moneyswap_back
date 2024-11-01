c245576290f0838c6f9d4832b449d6a03f497a36
Vlad Yashenkov <yashenkov.v@yandex.ru>
2024-11-01 12:12:30 +0300

    test: test send_mass

--- django_fastapi/seo_admin/static/seo_admin/js/test_1.js
+++ django_fastapi/seo_admin/static/seo_admin/js/test_1.js
@@ -1,5 +1,12 @@
 window.addEventListener('load', function () {
     let is_detail_view = document.getElementsByClassName('change-form').length;
+    let name = document.getElementById('id_name');
+
+    if (name) {
+        let name_send = name.textContent;
+        console.log(name_send);
+    }
+
     console.log(is_detail_view);
     if (is_detail_view != 0) {
 
@@ -8,7 +15,7 @@ window.addEventListener('load', function () {
         let mass_send_button = document.createElement('button');
         mass_send_button.textContent = 'mass_send';
         mass_send_button.onclick = function() {
-            let data = fetch('https://api.moneyswap.online/send_mass_message')
+            let data = fetch(`https://api.moneyswap.online/send_mass_message?name=${name_send}`)
             .then(resp => {
                 resp.text().then(console.log)
             })
