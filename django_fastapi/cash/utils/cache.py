850eb230876d1526a5381584c71e1ac092887330
Vlad Yashenkov <yashenkov.v@yandex.ru>
2024-12-20 11:48:01 +0300

    test: test

--- django_fastapi/cash/utils/cache.py
+++ django_fastapi/cash/utils/cache.py
@@ -174,7 +174,7 @@ def get_or_set_cache_available_countries3(request: Request):
                                                 .select_related('country')\
                                                 .prefetch_related('cash_directions',
                                                                 'partner_cities')\
-                                                .annotate(partner_direction_count=Count('partner_cities',
+                                                .annotate(partner_direction_count=Count('partner_cities__partner_directions',
                                                                                         filter=Q(partner_cities__partner_directions__is_active=True)))\
                                                 .annotate(direction_count=Count('cash_directions',
                                                                                 filter=Q(cash_directions__is_active=True)))\
@@ -213,11 +213,12 @@ def get_or_set_cache_available_countries3(request: Request):
                                                                     filter=Q(cities__cash_directions__is_active=True)))\
                                     .annotate(country_direction_count=Count('partner_countries__partner_directions',
                                                                             filter=Q(partner_countries__partner_directions__is_active=True)))\
-                                    .annotate(partner_direction_count=Count('cities__partner_cities__partner_directions',
-                                                                    filter=Q(cities__partner_cities__partner_directions__is_active=True)))\
-                                    .filter(Q(direction_count__gt=0) | Q(country_direction_count__gt=0) | Q(partner_direction_count__gt=0))\
+                                    .filter(Q(direction_count__gt=0) | Q(country_direction_count__gt=0))\
                                     .order_by('name')\
                                     .all()
+                                    # .annotate(partner_direction_count=Count('cities__partner_cities__partner_directions',
+                                    #                                 filter=Q(cities__partner_cities__partner_directions__is_active=True)))\
+                                    # | Q(partner_direction_count__gt=0)
 
 
         if not countries:
