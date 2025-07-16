63550f0ff07c399c9592e7f54107bbb285c317db
Vlad Yashenkov <yashenkov.v@yandex.ru>
2025-07-16 14:13:01 +0300

    feat: add /delete_admin_exchange_order and /switch_notification_activity partner API endpoints

--- django_fastapi/general_models/admin.py
+++ django_fastapi/general_models/admin.py
@@ -804,6 +804,10 @@ class ExchangeAdminAdmin(admin.ModelAdmin):
         'exchange_marker',
     )
 
+    raw_id_fields = (
+        'user',
+    )
+
     def get_queryset(self, request):
         return super().get_queryset(request).select_related('user')
     # readonly_fields = (
